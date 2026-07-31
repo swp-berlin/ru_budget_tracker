b8965df8a2af983e07c6bd12fc3d4d9805893da4
Paul Bochtler <paulbochtler@swp-berlin.org>
2026-07-31 18:34:18 +0200

    feat: show program code in front of program name in treemap labels

--- src/utils/fetch_treemap.py
+++ src/utils/fetch_treemap.py
@@ -145,10 +145,18 @@ def _build_dimension_name_column(translated: bool = False):
         SQLAlchemy column expression for concatenated dimension name.
     """
     name_field = Dimension.name_translated if translated else Dimension.name
-    return case(
-        (Dimension.type == "PROGRAM", name_field),
-        else_=func.CONCAT(Dimension.original_identifier, " ", name_field),
+    identifier = case(
+        (
+            Dimension.original_identifier.like("%-%"),
+            func.substr(
+                Dimension.original_identifier,
+                1,
+                func.instr(Dimension.original_identifier, "-") - 1,
+            ),
+        ),
+        else_=Dimension.original_identifier,
     )
+    return func.CONCAT(identifier, " ", name_field)
 
 
 @lru_cache(maxsize=1)
